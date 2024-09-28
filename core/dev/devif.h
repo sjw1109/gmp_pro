b2fda1d12cadaf5acecc6deaa2925c361e4ade31
javnson <65205730+javnson@users.noreply.github.com>
2024-09-28 17:00:24 +0800

    PMSM controller format upgrade

--- core/dev/devif.h
+++ core/dev/devif.h
@@ -1,4 +1,4 @@
-// This file will define all the device interface (Dev-IF) 
+// This file will define all the device interface (Dev-IF)
 
 #ifndef _FILE_DEVICE_INTERFACE_H_
 #define _FILE_DEVICE_INTERFACE_H_
@@ -8,127 +8,131 @@ extern "C"
 {
 #endif // __cplusplus
 
-// This file provide a set of structure type which owns a suffix ift (interface type).
+    // This file provide a set of structure type which owns a suffix ift (interface type).
 
-// This file is independent of the other modules.
+    // This file is independent of the other modules.
 
+    // ....................................................................//
 
-// ....................................................................//
-// half Duplex communication
-// such as half duplex SPI, half duplex UART (RS485)
-typedef struct _tag_half_duplex_channel_t
-{
-    // this is a pointer of data to be transmit
-    data_gt* buf;
+    /**
+     * @brief half Duplex communication.
+     * such as half duplex SPI, half duplex UART (RS485)
+     */
+    typedef struct _tag_half_duplex_channel_t
+    {
+        /**
+         * @brief this is a pointer of data to be transmit
+         *
+         */
+        data_gt *buf;
 
-    // length of the message
-    size_gt length;
+        // length of the message
+        size_gt length;
 
-    // capacity of the message
-    size_gt capacity;
+        // capacity of the message
+        size_gt capacity;
 
-}half_duplex_ift;
+    } half_duplex_ift;
 
-// ....................................................................//
-// Duplex communication
-// Such as SPI, UART, USART
-typedef struct _tag_duplex_channel_t
-{
-    // transmit buffer
-    data_gt* tx_buf;
+    // ....................................................................//
+    // Duplex communication
+    // Such as SPI, UART, USART
+    typedef struct _tag_duplex_channel_t
+    {
+        // transmit buffer
+        data_gt *tx_buf;
 
-    // receive buffer
-    data_gt* rx_buf;
+        // receive buffer
+        data_gt *rx_buf;
 
-    // length of the buffer (tx and rx buffer have the same length)
-    size_gt length;
+        // length of the buffer (tx and rx buffer have the same length)
+        size_gt length;
 
-    // capacity of the buffer (tx and rx buf have the same capacity)
-    size_gt capacity;
-    
-}duplex_ift;
+        // capacity of the buffer (tx and rx buf have the same capacity)
+        size_gt capacity;
 
-// ....................................................................//
-// half duplex with Address
-// such as IIC
-typedef struct _tag_half_duplex_with_addr_channel_t
-{
-    // Address of device
-    addr32_gt address;
+    } duplex_ift;
 
-    // message buffer
-    data_gt* msg;
+    // ....................................................................//
+    // half duplex with Address
+    // such as IIC
+    typedef struct _tag_half_duplex_with_addr_channel_t
+    {
+        // Address of device
+        addr32_gt address;
 
-    // length of buffer
-    size_gt length;
+        // message buffer
+        data_gt *msg;
 
-}half_duplex_with_addr_ift;
+        // length of buffer
+        size_gt length;
 
+    } half_duplex_with_addr_ift;
 
-// ....................................................................//
-// half duplex with dual address for IIC memeory mode
-typedef struct _tag_iic_memory_channel_t
-{
-    // device address 
-    addr32_gt dev_addr;
+    // ....................................................................//
+    // half duplex with dual address for IIC memeory mode
+    typedef struct _tag_iic_memory_channel_t
+    {
+        // device address
+        addr32_gt dev_addr;
 
-    // device memory address
-    addr32_gt mem_addr;
+        // device memory address
+        addr32_gt mem_addr;
 
-    // message buffer
-    data_gt* msg;
+        // message buffer
+        data_gt *msg;
 
-    // length of buffer
-    size_gt length;
+        // length of buffer
+        size_gt length;
 
-}iic_memory_ift;
+    } iic_memory_ift;
 
-// ....................................................................//
-// CAN interface
-typedef struct _tag_can_channel_t
-{
-    // can address
-    // std ID is a 11-bit number, 0 ~ 0x7FF
-    // ext ID is a 29-bit number, 0 ~ 0x1FFF_FFFF
-    addr32_gt id; 
+    // ....................................................................//
+    // CAN interface
+    typedef struct _tag_can_channel_t
+    {
+        // can address
+        // std ID is a 11-bit number, 0 ~ 0x7FF
+        // ext ID is a 29-bit number, 0 ~ 0x1FFF_FFFF
+        addr32_gt id;
 
-    // can frame type
-    // RTR flag: remote flag
-    // IDE flag; extension flag
-    uint32_t properties;
+        // can frame type
+        // RTR flag: remote flag
+        // IDE flag; extension flag
+        uint32_t properties;
 
-    // can data length
-    fast8_gt length;
+        // can data length
+        fast8_gt length;
 
-    // can data buffer
-    data_gt data[8];
+        // can data buffer
+        data_gt data[8];
 
-}can_ift;
+    } can_ift;
 
-// ....................................................................//
-// Slot interface
-// This struct contains a fixed length buffer
-typedef struct _tag_slot_interface_t
-{
-    // slot size
-    addr_gt length;
+    // ....................................................................//
+    // Slot interface
+    // This struct contains a fixed length buffer
+    typedef struct _tag_slot_interface_t
+    {
+        // slot size
+        addr_gt length;
 
-    // slot buffer pointer
-    data_gt* buffer;
-}interface_slot_ift;
+        // slot buffer pointer
+        data_gt *buffer;
+    } interface_slot_ift;
 
-// ....................................................................//
-// Strip for slot interface
-// This struct contains a list of slots.
-typedef struct _tag_strip_interface_t
-{
-    // strip size
-    // how many slots the slot has.
-    uint32_t size;
+    // ....................................................................//
+    // Strip for slot interface
+    // This struct contains a list of slots.
+    typedef struct _tag_strip_interface_t
+    {
+        // strip size
+        // how many slots the slot has.
+        uint32_t size;
 
-    // pointer list of slot interface
-    interface_slot_ift** slot_it;
-}interface_strip_ift;
+        // pointer list of slot interface
+        interface_slot_ift **slot_it;
+    } interface_strip_ift;
 
 #ifdef __cplusplus
 }
--- ctl/suite/pmsm/pmsm_speed_closeloop/pmsm_speed_closeloop.h
+++ ctl/suite/pmsm/pmsm_speed_closeloop/pmsm_speed_closeloop.h
@@ -1,4 +1,13 @@
-// This is an example of PMSM controller via Controller Nano framework
+/**
+ * @file pmsm_speed_closeloop.h
+ * @author Javnson (javnson@zju.edu.cn)
+ * @brief his is an example of PMSM controller via Controller Nano framework
+ * @version 0.1
+ * @date 2024-09-28
+ *
+ * @copyright Copyright GMP(c) 2024
+ *
+ */
 
 #include <ctl/framework/ctl_nano.h>
 #include <ctl/suite/pmsm/pmsm.ctl.h>
@@ -6,51 +15,89 @@
 #ifndef _FILE_PMSM_SPEED_CLOSELOOP_H_
 #define _FILE_PMSM_SPEED_CLOSELOOP_H_
 
-// PMSM controller top module
-typedef struct _tag_pmsm_ctl_object
+#if defined __cplusplus
+extern "C"
 {
-	// Controller Framework
-	ctl_object_nano_t base;
+#endif // __cplusplus
 
+    /**
+     * @brief PMSM controller top module
+     *
+     */
+    typedef struct _tag_pmsm_ctl_object
+    {
+        // Controller Framework
+        ctl_object_nano_t base;
 
-	// controller entity
-	pmsm_ctl_entity_t ctrl;
+        // controller entity
+        pmsm_ctl_entity_t ctrl;
 
-	// Monitor Objects
+        // Monitor Objects
 
-	// Protect Objects
+        // Protect Objects
 
-	// ADC Calibrate Objects
-	adc_bias_calibrator_t calibrator;
-	uint32_t calibrae_progress;
+        // ADC Calibrate Objects
+        adc_bias_calibrator_t calibrator;
+        uint32_t calibrae_progress;
 
-}pmsm_ctl_object_t;
+    } pmsm_ctl_object_t;
 
+    //////////////////////////////////////////////////////////////////////////
+    // The following functions should implemented by user
+    // copy from <ctl_nano.h>
 
-//////////////////////////////////////////////////////////////////////////
-// The following functions should implemented by user
-// copy from <ctl_nano.h>
+    /**
+     * @brief deal with all the ADC, Motor Encoder and other input messages
+     *
+     * @param pctl_obj controller objects
+     */
+    void ctl_input_stage_routine(ctl_object_nano_t *pctl_obj);
 
-// deal with all the adc and other input messages
-void ctl_input_stage_routine(ctl_object_nano_t* pctl_obj);
+    /**
+     * @brief implement all the controller routine
+     *
+     * @param pctl_obj controller objects
+     */
+    void ctl_core_stage_routine(ctl_object_nano_t *pctl_obj);
 
-// implement all the controller routine
-void ctl_core_stage_routine(ctl_object_nano_t* pctl_obj);
+    /**
+     * @brief output all the PWM and other digital or analog signal
+     *
+     * @param pctl_obj controller objects
+     */
+    void ctl_output_stage_routine(ctl_object_nano_t *pctl_obj);
 
-// output all the PWM and other digital or analog signal
-void ctl_output_stage_routine(ctl_object_nano_t* pctl_obj);
+    // request other information via peripheral, for instance SPI.
+    /**
+     * @brief output all the PWM and other digital or analog signal
+     *
+     * @param pctl_obj controller objects
+     */
+    void ctl_request_stage_routine(ctl_object_nano_t *pctl_obj);
 
-// request other information via peripheral, for instance SPI.
-void ctl_request_stage_routine(ctl_object_nano_t* pctl_obj);
+    /**
+     * @brief enable PWM output
+     *
+     * @param pctl_obj controller objects
+     */
+    void controller_output_enable(ctl_object_nano_t *pctl_obj);
 
-// enable PWM output
-void controller_output_enable(ctl_object_nano_t* pctl_obj);
+    /**
+     * @brief disable PWM output
+     *
+     * @param pctl_obj controller objects
+     */
+    void controller_output_disable(ctl_object_nano_t *pctl_obj);
 
-// disable PWM output
-void controller_output_disable(ctl_object_nano_t* pctl_obj);
+    /**
+     * @brief controller monitor routine
+     *
+     * @param pctl_obj controller objects
+     */
+    void controller_monitor_routine(ctl_object_nano_t *pctl_obj);
 
-// controller monitor routine
-void controller_monitor_routine(ctl_object_nano_t* pctl_obj);
-
+#if defined __cplusplus
+}
+#endif // __cplusplus
 
 #endif // _FILE_PMSM_SPEED_CLOSELOOP_H_
