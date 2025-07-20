f778c008129b6aea092a0053d2b356b0314f5662
javnson <65205730+javnson@users.noreply.github.com>
2025-07-20 22:05:58 +0800

    update single phase DC/AC inverter model

--- ctl/suite/pgs_1ph_dcac_inv/implement/common/ctl_main.c
+++ ctl/suite/pgs_1ph_dcac_inv/implement/common/ctl_main.c
@@ -20,30 +20,35 @@
 
 #include <xplt.peripheral.h>
 
-#include <ctl/component/intrinsic/continuous/continuous_pid.h>
-pid_regular_t current_pid, voltage_pid;
-pid_regular_t sinv_vlotage_pid;
-ctrl_gt pwm_out_pu;
+#include <ctl/component/digital_power/topology_preset/single_phase_dc_ac.h>
 
-ctl_single_phase_pll spll;
-ctrl_gt ac_input;
-
-ctrl_gt sinv_pwm_pu[2];
-pr_ctrl_t sinv_pr_base;
-qpr_ctrl_t sinv_qpr_base;
-ctrl_gt sinv_current_ref;
-
-ctl_src_rg_t rg;
+sinv_ctrl_t sinv_ctrl;
 
 //
-adc_bias_calibrator_t adc_calibrator;
-fast_gt flag_enable_adc_calibrator = 0;
-fast_gt index_adc_calibrator = 0;
-
-ctrl_gt modulate_target;
-
-
-ctrl_gt v_set = float2ctrl(0.02);
+// #include <ctl/component/intrinsic/continuous/continuous_pid.h>
+// pid_regular_t current_pid, voltage_pid;
+// pid_regular_t sinv_vlotage_pid;
+// ctrl_gt pwm_out_pu;
+//
+// ctl_single_phase_pll spll;
+// ctrl_gt ac_input;
+//
+// ctrl_gt sinv_pwm_pu[2];
+// pr_ctrl_t sinv_pr_base;
+// qpr_ctrl_t sinv_qpr_base;
+// ctrl_gt sinv_current_ref;
+//
+// ctl_src_rg_t rg;
+//
+////
+// adc_bias_calibrator_t adc_calibrator;
+// fast_gt flag_enable_adc_calibrator = 0;
+// fast_gt index_adc_calibrator = 0;
+//
+// ctrl_gt modulate_target;
+//
+//
+// ctrl_gt v_set = float2ctrl(0.02);
 
 // enable motor running
 volatile fast_gt flag_enable_system = 0;
@@ -52,241 +57,139 @@ volatile fast_gt flag_error = 0;
 // CTL initialize routine
 void ctl_init()
 {
-    ctl_init_pid(
-        // continuous pid handle
-        &current_pid,
-        // PID parameters
-        1, 0.002, 0,
-        // controller frequency
-        20e3);
-
-    ctl_init_pid(
-        // continuous pid handle
-        &sinv_vlotage_pid,
-        // PID parameters
-        0.8/4, 0.1, 0,
-        // controller frequency
-        20e3);
-
-    ctl_init_pid(
-        // continuous pid handle
-        &voltage_pid,
-        // PID parameters
-        1, 0.01, 0,
-        // controller frequency
-        20e3);
-
-    ctl_init_single_phase_pll(
-        // handle of Single phase PLL object
-        &spll,
-        // gain of SPLL module
-        0.75f/20,
-        // Time of integrate
-        //0.10f,
-        1000.0f,
-        // filter cut frequency
-        9.0f,
-        // Target frequency, grid frequency, Hz
-        50.0f,
-        // isr frequency, Hz
-        20e3);
-
-    ctl_init_pr_controller(&sinv_pr_base, 0.0001, 1000, 50, 20e3);
-    ctl_init_qpr_controller(&sinv_qpr_base, 0.00000001, 500/10, 50, 5 ,20e3);
-    ctl_init_qpr_controller(&qpr_test, 0.01, 0.08, 50, 5, 20e3);
-
-    ctl_init_ramp_gen_via_amp_freq(&rg, 20e3, 50, 1, 0);
-
-    //    // setup ADC calibrate
-    //    ctl_filter_IIR2_setup_t adc_calibrator_filter;
-    //    adc_calibrator_filter.filter_type = FILTER_IIR2_TYPE_LOWPASS;
-    //    adc_calibrator_filter.fc = 20;
-    //    adc_calibrator_filter.fs = CONTROLLER_FREQUENCY;
-    //    adc_calibrator_filter.gain = 1;
-    //    adc_calibrator_filter.q = 0.707f;
-    //    ctl_init_adc_calibrator(&adc_calibrator, &adc_calibrator_filter);
-    //
-    // #ifdef PMSM_CTRL_USING_QEP_ENCODER
-    //    // init Auto - turn encoder
-    //    ctl_init_autoturn_pos_encoder(&pos_enc, MOTOR_PARAM_POLE_PAIRS, MTR_ENCODER_LINES);
-    //    // Set encoder offset
-    //    ctl_set_autoturn_pos_encoder_offset(&pos_enc, MTR_ENCODER_OFFSET);
-    // #endif // PMSM_CTRL_USING_QEP_ENCODER
-    //
-    //    flag_enable_system = 0;
-    //
-    //    // create a speed observer by position encoder
-    //    ctl_init_spd_calculator(
-    //        // attach position with speed encoder
-    //        &spd_enc, pmsm_ctrl.mtr_interface.position,
-    //        // set spd calculator parameters
-    //        CONTROLLER_FREQUENCY, 5, MOTOR_PARAM_MAX_SPEED, 1, 150);
-    //
-    // #if defined OPENLOOP_CONST_FREQUENCY
-    //    ctl_init_const_f_controller(&const_f, 20, CONTROLLER_FREQUENCY);
-    // #else  // OPENLOOP_CONST_FREQUENCY
-    //    // frequency target 20 Hz, frequency slope 40 Hz/s
-    //    ctl_init_const_slope_f_controller(&slope_f, 20.0f, 40.0f, CONTROLLER_FREQUENCY);
-    // #endif // OPENLOOP_CONST_FREQUENCY
-    //
-    //    // attach a speed encoder object with motor controller
-    //    ctl_attach_mtr_velocity(&pmsm_ctrl.mtr_interface, &spd_enc.encif);
-    //
-    //    // set pmsm_ctrl parameters
-    //    pmsm_bare_controller_init_t pmsm_ctrl_init;
-    //
-    //    pmsm_ctrl_init.fs = CONTROLLER_FREQUENCY;
-    //
-    //    // current pid controller parameters
-    //    pmsm_ctrl_init.current_pid_gain = (parameter_gt)(MOTOR_PARAM_LS * MTR_CTRL_CURRENT_LOOP_BW * 2 * PI *
-    //                                                     MTR_CTRL_VOLTAGE_BASE / MTR_CTRL_CURRENT_BASE);
-    //    pmsm_ctrl_init.current_Ti = (parameter_gt)(MOTOR_PARAM_LS / MOTOR_PARAM_RS);
-    //    pmsm_ctrl_init.current_Td = 0;
-    //    pmsm_ctrl_init.voltage_limit_min = float2ctrl(-1.0);
-    //    pmsm_ctrl_init.voltage_limit_max = float2ctrl(1.0);
-    //
-    //    // speed pid controller parameters
-    //    pmsm_ctrl_init.spd_ctrl_div = SPD_CONTROLLER_PWM_DIVISION;
-    //    pmsm_ctrl_init.spd_pid_gain = (parameter_gt)(0.2);
-    //    pmsm_ctrl_init.spd_Ti = (parameter_gt)(4.0f / MTR_CTRL_SPEED_LOOP_BW);
-    //    pmsm_ctrl_init.spd_Td = 0;
-    //    pmsm_ctrl_init.current_limit_min = float2ctrl(-0.45);
-    //    pmsm_ctrl_init.current_limit_max = float2ctrl(0.45);
-    //
-    //    // accelerator parameters
-    //    pmsm_ctrl_init.acc_limit_min = -150.0f;
-    //    pmsm_ctrl_init.acc_limit_max = 150.0f;
-    //
-    //    // init the PMSM controller
-    //    ctl_init_pmsm_bare_controller(&pmsm_ctrl, &pmsm_ctrl_init);
-
-    // BUG TI cannot print out sizeof() result if no type is specified.
-    // gmp_base_print(TEXT_STRING("PMSM SERVO struct has been inited, size :%d\r\n"), (int)sizeof(pmsm_ctrl_init));
-
-    // #if (BUILD_LEVEL == 1)
-    // #if defined OPENLOOP_CONST_FREQUENCY
-    //     ctl_attach_mtr_position(&pmsm_ctrl.mtr_interface, &const_f.enc);
-    // #else  // OPENLOOP_CONST_FREQUENCY
-    //     ctl_attach_mtr_position(&pmsm_ctrl.mtr_interface, &slope_f.enc);
-    // #endif // OPENLOOP_CONST_FREQUENCY
-    //
-    //     ctl_pmsm_ctrl_voltage_mode(&pmsm_ctrl);
-    //     ctl_set_pmsm_ctrl_vdq_ff(&pmsm_ctrl, float2ctrl(0.2), float2ctrl(0.2));
-    //
-    // #elif (BUILD_LEVEL == 2)
-    // #if defined OPENLOOP_CONST_FREQUENCY
-    //     ctl_attach_mtr_position(&pmsm_ctrl.mtr_interface, &const_f.enc);
-    // #else  // OPENLOOP_CONST_FREQUENCY
-    //     ctl_attach_mtr_position(&pmsm_ctrl.mtr_interface, &slope_f.enc);
-    // #endif // OPENLOOP_CONST_FREQUENCY
-    //     ctl_pmsm_ctrl_current_mode(&pmsm_ctrl);
-    //     ctl_set_pmsm_ctrl_idq_ff(&pmsm_ctrl, float2ctrl(0.1), float2ctrl(0.1));
-    //
-    // #elif (BUILD_LEVEL == 3)
-    //
-    //     ctl_pmsm_ctrl_current_mode(&pmsm_ctrl);
-    //     ctl_set_pmsm_ctrl_idq_ff(&pmsm_ctrl, float2ctrl(0.1), float2ctrl(0.05));
-    //
-    // #elif (BUILD_LEVEL == 4)
-    //
-    //     ctl_pmsm_ctrl_velocity_mode(&pmsm_ctrl);
-    //     ctl_set_pmsm_ctrl_speed(&pmsm_ctrl, float2ctrl(0.25));
-    // #endif // BUILD_LEVEL
-
-    // if in simulation mode, enable system
-#if !defined SPECIFY_PC_ENVIRONMENT
     // stop here and wait for user start the motor controller
-		ctl_disable_output();
+    ctl_disable_output();
+
+    sinv_init_t init;
+
+    init.base_freq = 50.0f;
+
+    // voltage controller parameters
+    init.v_ctrl_kp = 0.4f;
+    init.v_ctrl_Ti = 0.2f;
+    init.v_ctrl_Td = 0;
+
+    // PLL parameters
+    init.pll_ctrl_kp = 0.75f / 20;
+    init.pll_ctrl_Ti = 1000.0f;
+    init.pll_ctrl_cut_freq = 9.0f;
+
+    // current controller parameters
+    init.i_ctrl_kp = 0.00000001f;
+    init.i_ctrl_kr = 20.0f;
+    init.i_ctrl_cut_freq = 8.0f;
+
+    // harmonic controller parameters
+    init.harm_ctrl_kr_3 = 1.0f;
+    init.harm_ctrl_cut_freq_3 = 5.0f;
+    init.harm_ctrl_kr_5 = 1.0f;
+    init.harm_ctrl_cut_freq_5 = 5.0f;
+    init.harm_ctrl_kr_7 = 1.0f;
+    init.harm_ctrl_cut_freq_7 = 5.0f;
+
+    // adc input filter cut frequency
+    init.adc_filter_fc = 1000.0;
+
+    // controller frequency
+    init.f_ctrl = 20e3;
+
+    // init sinv Controller
+    ctl_init_sinv_ctrl(&sinv_ctrl, &init);
+
+#if BUILD_LEVEL == 1
+    // Voltage open loop
+    ctl_set_sinv_openloop_inverter(&sinv_ctrl);
+    ctl_set_sinv_current_ref(&sinv_ctrl, float2ctrl(0.1));
+    ctl_set_sinv_freerun(&sinv_ctrl);
+    ctl_disable_sinv_harm_ctrl(&sinv_ctrl);
+
+#elif BUILD_LEVEL == 2
+    // current close loop
+    ctl_set_sinv_current_closeloop_inverter(&sinv_ctrl);
+    ctl_set_sinv_current_ref(&sinv_ctrl, float2ctrl(0.1));
+    ctl_set_sinv_freerun(&sinv_ctrl);
+    ctl_disable_sinv_harm_ctrl(&sinv_ctrl);
+
+#elif BUILD_LEVEL == 3
+
+    // current close loop, with harm control
+    ctl_set_sinv_current_closeloop_inverter(&sinv_ctrl);
+    ctl_set_sinv_current_ref(&sinv_ctrl, float2ctrl(0.1));
+    ctl_set_sinv_freerun(&sinv_ctrl);
+    ctl_enable_sinv_harm_ctrl(&sinv_ctrl);
+
+#elif BUILD_LEVEL == 4
+    // rectifier voltage loop, without harm control
+    ctl_set_sinv_voltage_closeloop_rectifier(&sinv_ctrl);
+    ctl_set_sinv_voltage_ref(&sinv_ctrl, float2ctrl(0.4));
+    ctl_disable_sinv_harm_ctrl(&sinv_ctrl);
+    ctl_set_sinv_pll(&sinv_ctrl);
+
+#elif BUILD_LEVEL == 5
+    // rectifier voltage loop, with harm control
+    ctl_set_sinv_voltage_closeloop_rectifier(&sinv_ctrl);
+    ctl_set_sinv_voltage_ref(&sinv_ctrl, float2ctrl(0.4));
+    ctl_enable_sinv_harm_ctrl(&sinv_ctrl);
+    ctl_set_sinv_pll(&sinv_ctrl);
+
+#elif BUILD_LEVEL == 6
+    // inverter voltage loop, without harm control
+    ctl_set_sinv_voltage_closeloop_inverter(&sinv_ctrl);
+    ctl_set_sinv_voltage_ref(&sinv_ctrl, float2ctrl(0.4));
+    ctl_disable_sinv_harm_ctrl(&sinv_ctrl);
+    ctl_set_sinv_pll(&sinv_ctrl);
+
+#elif BUILD_LEVEL == 7
+    // inverter voltage loop, with harm control
+    ctl_set_sinv_voltage_closeloop_inverter(&sinv_ctrl);
+    ctl_set_sinv_voltage_ref(&sinv_ctrl, float2ctrl(0.4));
+    ctl_enable_sinv_harm_ctrl(&sinv_ctrl);
+    ctl_set_sinv_pll(&sinv_ctrl);
+
+#endif // BUILD_LEVEL
+
+    ctl_disable_output();
+
+    // if in simulation mode, enable system automatically
+#if !defined SPECIFY_PC_ENVIRONMENT
+
     while (flag_enable_system == 0)
     {
     }
 
 #endif // SPECIFY_PC_ENVIRONMENT
-
-    ctl_enable_output();
-
-    // Debug mode online the controller
-    // ctl_enable_pmsm_ctrl(&pmsm_ctrl);
-
-#if defined SPECIFY_ENABLE_ADC_CALIBRATE
-    // Enable ADC calibrate
-    flag_enable_adc_calibrator = 1;
-    index_adc_calibrator = 0;
-
-    // Select ADC calibrate
-    // ctl_disable_pmsm_ctrl_output(&pmsm_ctrl);
-    ctl_enable_adc_calibrator(&adc_calibrator);
-#endif // SPECIFY_ENABLE_ADC_CALIBRATE
 }
 
 //////////////////////////////////////////////////////////////////////////
 // endless loop function here
 
 uint16_t sgen_out = 0;
+fast_gt firsttime_flag = 0;
+fast_gt startup_flag = 0;
+fast_gt started_flag = 0;
+time_gt tick_bias = 0;
 
 void ctl_mainloop(void)
 {
-    // int spd_target = gmp_base_get_system_tick() / 100 - 4;
+    // first time flag
+    if (firsttime_flag == 0)
+    {
+        tick_bias = gmp_base_get_system_tick();
+        firsttime_flag = 1;
+    }
 
-    // ctl_set_pmsm_ctrl_speed(&pmsm_ctrl, float2ctrl(0.1) * spd_target - float2ctrl(1.0));
+    // a delay of 500ms
+    if ((started_flag == 0) && ((gmp_base_get_system_tick() - tick_bias) > 500) && (startup_flag == 0))
+    {
+        startup_flag = 1;
+    }
 
-    //
-    // Judge if PWM is enabled
-    //
-    // if (pmsm_ctrl.flag_enable_output)
-    //{
-    //    ctl_enable_output();
-    //}
-    // else
-    //{
-    //    ctl_disable_output();
-    //}
-
-    //
-    // ADC Auto calibrate
-    //
-    // if (flag_enable_adc_calibrator)
-    //{
-    //    if (ctl_is_adc_calibrator_cmpt(&adc_calibrator) && ctl_is_adc_calibrator_result_valid(&adc_calibrator))
-    //    {
-    //        // set_adc_bias_via_channel(index_adc_calibrator, ctl_get_adc_calibrator_result(&adc_calibrator));
-
-    //        if (index_adc_calibrator == 3) // dc bus calibrate
-    //        {
-    //            idc.bias = idc.bias + ctl_div(ctl_get_adc_calibrator_result(&adc_calibrator), idc.gain);
-
-    //            flag_enable_adc_calibrator = 0;
-
-    //            // enable pmsm controller
-    //            ctl_enable_pmsm_ctrl_output(&pmsm_ctrl);
-    //        }
-    //        // index_adc_calibrator == 2 ~ 0, for Iabc
-    //        else
-    //        {
-    //            // iabc get result
-    //            iabc.bias[index_adc_calibrator] =
-    //                iabc.bias[index_adc_calibrator] +
-    //                ctl_div(ctl_get_adc_calibrator_result(&adc_calibrator), iabc.gain[index_adc_calibrator]);
-
-    //            // move to next position
-    //            index_adc_calibrator += 1;
-
-    //            // clear calibrator
-    //            ctl_clear_adc_calibrator(&adc_calibrator);
-
-    //            // enable calibrator to next position
-    //            ctl_enable_adc_calibrator(&adc_calibrator);
-    //        }
-
-    //        if (index_adc_calibrator > MTR_ADC_IDC)
-    //            flag_enable_adc_calibrator = 0;
-    //    }
-    //}
-		
-		if(sinv_ig.control_port.value>=2)
-		{
-			ctl_disable_output();
-		}
+    // judge if PLL is close to target
+    if ((started_flag == 0) && (startup_flag == 1) && sinv_ctrl.spll.spll_ctrl.out < float2ctrl(0.001))
+    {
+        ctl_enable_output();
+        started_flag = 1;
+    }
 
     return;
 }
