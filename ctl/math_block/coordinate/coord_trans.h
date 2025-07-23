5b43b8bb05a9167ae6a5ed921e9b312b9d09feb1
javnson <65205730+javnson@users.noreply.github.com>
2025-07-23 15:32:56 +0800

    Update Coordinate transform blocks

--- ctl/math_block/coordinate/coord_trans.h
+++ ctl/math_block/coordinate/coord_trans.h
@@ -2,8 +2,8 @@
  * @file coord_trans.h
  * @author Javnson (javnson@zju.edu.cn)
  * @brief
- * @version 0.1
- * @date 2024-09-30
+ * @version 1.0
+ * @date 2025-07-23
  *
  * @copyright Copyright GMP(c) 2024
  *
@@ -47,6 +47,11 @@ enum ALPHA_BETA_ENUM
     phase_alpha = 0,
     phase_beta = 1
 };
+enum LINE_VOLTAGE_ENUM
+{
+    phase_UAB = 0,
+    phase_UBC = 1
+};
 
 enum PHASOR_ENUM
 {
@@ -79,10 +84,14 @@ enum PHASOR_ENUM
 
 // angle to 2-d phasor
 GMP_STATIC_INLINE
-void ctl_set_phasor_via_angle(ctrl_gt angle, GMP_CTL_OUTPUT_TAG ctl_vector2_t *phasor)
+void ctl_set_phasor_via_angle(
+    // angle
+    ctrl_gt angle,
+    // output phasor
+    GMP_CTL_OUTPUT_TAG ctl_vector2_t *phasor)
 {
-    phasor->dat[0] = ctl_sin(angle);
-    phasor->dat[1] = ctl_cos(angle);
+    phasor->dat[phasor_sin] = ctl_sin(angle);
+    phasor->dat[phasor_cos] = ctl_cos(angle);
 
     // ctrl_gt theta = ctl_mul(theta, GMP_CONST_1_OVER_2PI);
 
@@ -95,7 +104,11 @@ void ctl_set_phasor_via_angle(ctrl_gt angle, GMP_CTL_OUTPUT_TAG ctl_vector2_t *p
 // ABC to alpha-beta
 // Constant amplitude transformation
 GMP_STATIC_INLINE
-void ctl_ct_clark(ctl_vector3_t *abc, GMP_CTL_OUTPUT_TAG ctl_vector3_t *ab)
+void ctl_ct_clark(
+    // ABC
+    ctl_vector3_t *abc,
+    // output alpha beta
+    GMP_CTL_OUTPUT_TAG ctl_vector3_t *ab)
 {
     ctrl_gt k_alpha = GMP_CONST_ABC2AB_ALPHA; // 2/3
     ctrl_gt k_beta = GMP_CONST_ABC2AB_BETA;   // 1/sqrt(3)
@@ -103,74 +116,119 @@ void ctl_ct_clark(ctl_vector3_t *abc, GMP_CTL_OUTPUT_TAG ctl_vector3_t *ab)
 
     // tex:
     //  $$i_\alpha = 2/3\times i_a - 1/3 \times i_b - 1/3 \times i_c $$
-    ab->dat[0] = ctl_mul(k_alpha, abc->dat[0] - ctl_div2(abc->dat[1] + abc->dat[2]));
+    ab->dat[phase_alpha] = ctl_mul(k_alpha, abc->dat[phase_A] - ctl_div2(abc->dat[phase_B] + abc->dat[phase_C]));
     // tex:
     //  $$i_\beta = \sqrt{3}/3\times i_b - \sqrt{3}/3\times i_c $$
-    ab->dat[1] = ctl_mul(k_beta, (abc->dat[1] - abc->dat[2]));
+    ab->dat[phase_beta] = ctl_mul(k_beta, (abc->dat[phase_B] - abc->dat[phase_C]));
     // tex:
     //  $$i_\beta = 1/3\times i_a + 1/3\times i_b + 1/3\times i_c $$
-    ab->dat[2] = ctl_mul(k_gamma, abc->dat[0] + abc->dat[1] + abc->dat[2]);
+    ab->dat[phase_0] = ctl_mul(k_gamma, abc->dat[phase_A] + abc->dat[phase_B] + abc->dat[phase_C]);
 }
 
-// clark coordinate axes transform
+// Clark coordinate axes transform
 // AB0(0 means C doesn't measure)
 GMP_STATIC_INLINE
-void ctl_ct_clark_2ph(ctl_vector3_t *ab0, GMP_CTL_OUTPUT_TAG ctl_vector3_t *ab)
+void ctl_ct_clark_2ph(
+    // A and B phase voltage/current
+    ctl_vector3_t *ab0,
+    // alpha beta
+    GMP_CTL_OUTPUT_TAG ctl_vector2_t *ab)
 {
     ctrl_gt k_alpha = GMP_CONST_AB02AB_ALPHA;
 
     // tex:
     //   $$i_\alpha = i_a $$
-    ab->dat[0] = ab0->dat[0];
+    ab->dat[phase_alpha] = ab0->dat[phase_A];
     // tex:
     //  $$i_\beta = 2/\sqrt{3}\times (i_a/2 + i_b) $$
-    ab->dat[1] = (ctrl_gt)ctl_mul(ctl_div2(ab0->dat[0]) + ab0->dat[1], k_alpha);
+    ab->dat[phase_beta] = (ctrl_gt)ctl_mul(ctl_div2(ab0->dat[phase_A]) + ab0->dat[phase_B], k_alpha);
+}
+
+// Clark coorinate axes transform
+//  uAB & uBC is line voltage
+GMP_STATIC_INLINE
+void ctl_ct_clark_from_line(
+    // uAB, uBC
+    ctl_vector2_t *u_line, 
+    // alpha beta
+    GMP_CTL_OUTPUT_TAG ctl_vector2_t *ab)
+{
+    ctrl_gt k_beta = GMP_CONST_ABC2AB_BETA;   // 1/sqrt(3)
+    ctrl_gt k_gamma = GMP_CONST_ABC2AB_GAMMA; // 1/3
+
     // tex:
-    //  $$ i_0 = 0$$
-    //  Restricted degrees of freedom
-    ab->dat[2] = float2ctrl(0.0f);
+    //  $$U_\alpha = \frac{1}{3}(2 U_{ab} + U_{bc})$$
+    ab->dat[phase_alpha] = ctl_mul(k_gamma, ctl_mul2(u_line->dat[phase_UAB]) + u_line->dat[phase_UBC]);
+
+    // tex:
+    //  $$U_\beta = \frac{1}{\sqrt{3}} U_{bc}$$
+    ab->dat[phase_beta] = ctl_mul(k_beta, u_line->dat[phase_UBC]);
 }
 
 // park coordinate axes transform
 // alpha-beta-0 to dq0
 GMP_STATIC_INLINE
-void ctl_ct_park(ctl_vector3_t *ab, ctl_vector2_t *phasor, GMP_CTL_OUTPUT_TAG ctl_vector3_t *dq0)
+void ctl_ct_park(
+    // alpha beta 0
+    ctl_vector3_t *ab, 
+    // phasor
+    ctl_vector2_t *phasor, 
+    // dq0
+    GMP_CTL_OUTPUT_TAG ctl_vector3_t *dq0)
 {
     // tex:
-    //  $$i_d = i_\alpha \times cos\;(\theta) + i_\beta \times sin\;(\theta) $$
-    dq0->dat[0] = ctl_mul(ab->dat[0], phasor->dat[1]) + ctl_mul(ab->dat[1], phasor->dat[0]);
+    //   $$i_d = i_\alpha \times cos\;(\theta) + i_\beta \times sin\;(\theta) $$
+    dq0->dat[phase_d] =
+        ctl_mul(ab->dat[phase_alpha], phasor->dat[phasor_cos]) + ctl_mul(ab->dat[phase_beta], phasor->dat[phasor_sin]);
     // tex:
     //$$i_q = - i_\alpha \times sin\;(\theta) + i_\beta \times cos\;(\theta)$$
-    dq0->dat[1] = -ctl_mul(ab->dat[0], phasor->dat[0]) + ctl_mul(ab->dat[1], phasor->dat[1]);
+    dq0->dat[phase_q] =
+        -ctl_mul(ab->dat[phase_alpha], phasor->dat[phasor_sin]) + ctl_mul(ab->dat[phase_beta], phasor->dat[phasor_cos]);
     // tex:
     //$$i_0 = i_0$$
-    dq0->dat[2] = ab->dat[2];
+    dq0->dat[phase_0] = ab->dat[phase_0];
 }
 
 // park coordinate axes transform
 // alpha-beta to dq
 GMP_STATIC_INLINE
-void ctl_ct_park2(ctl_vector2_t *ab, ctl_vector2_t *phasor, GMP_CTL_OUTPUT_TAG ctl_vector2_t *dq0)
+void ctl_ct_park2(
+    // alpha beta
+    ctl_vector2_t *ab,
+    // phasor
+    ctl_vector2_t *phasor,
+    // dq0
+    GMP_CTL_OUTPUT_TAG ctl_vector2_t *dq0)
 {
     // tex:
     //  $$i_d = i_\alpha \times cos\;(\theta) + i_\beta \times sin\;(\theta) $$
-    dq0->dat[0] = ctl_mul(ab->dat[0], phasor->dat[1]) + ctl_mul(ab->dat[1], phasor->dat[0]);
+    dq0->dat[phase_d] =
+        ctl_mul(ab->dat[phase_alpha], phasor->dat[phasor_cos]) + ctl_mul(ab->dat[phase_beta], phasor->dat[phasor_sin]);
     // tex:
     //$$i_q = - i_\alpha \times sin\;(\theta) + i_\beta \times cos\;(\theta)$$
-    dq0->dat[1] = -ctl_mul(ab->dat[0], phasor->dat[0]) + ctl_mul(ab->dat[1], phasor->dat[1]);
+    dq0->dat[phase_q] =
+        -ctl_mul(ab->dat[phase_alpha], phasor->dat[phasor_sin]) + ctl_mul(ab->dat[phase_beta], phasor->dat[phasor_cos]);
 }
 
 // ipark coordinate axes transform
 // DQ to alpha_beta
 GMP_STATIC_INLINE
-void ctl_ct_ipark(ctl_vector3_t *dq0, ctl_vector2_t *phasor, GMP_CTL_OUTPUT_TAG ctl_vector3_t *ab)
+void ctl_ct_ipark(
+    // dq0
+    ctl_vector3_t *dq0,
+    // phasor
+    ctl_vector2_t *phasor,
+    // alpha beta
+    GMP_CTL_OUTPUT_TAG ctl_vector3_t *ab)
 {
     // tex:
     //$$i_\alpha = i_d \times \cos\;(\theta) - i_q \times \sin\;(\theta) $$
-    ab->dat[phase_alpha] = ctl_mul(dq0->dat[0], phasor->dat[1]) - ctl_mul(dq0->dat[1], phasor->dat[0]);
+    ab->dat[phase_alpha] =
+        ctl_mul(dq0->dat[phase_d], phasor->dat[phasor_cos]) - ctl_mul(dq0->dat[phase_q], phasor->dat[phasor_sin]);
     // tex:
     //  $$ i_\beta = i_d \times \sin\;(\theta) + i_q \times \cos\;(\theta) $$
-    ab->dat[phase_beta] = ctl_mul(dq0->dat[0], phasor->dat[0]) + ctl_mul(dq0->dat[1], phasor->dat[1]);
+    ab->dat[phase_beta] =
+        ctl_mul(dq0->dat[phase_d], phasor->dat[phasor_sin]) + ctl_mul(dq0->dat[phase_q], phasor->dat[phasor_cos]);
     // tex:
     //$$i_0 = i_0$$
     ab->dat[phase_0] = dq0->dat[phase_0];
@@ -178,7 +236,11 @@ void ctl_ct_ipark(ctl_vector3_t *dq0, ctl_vector2_t *phasor, GMP_CTL_OUTPUT_TAG 
 
 // iClarke coordinate axes transform
 GMP_STATIC_INLINE
-void ctl_ct_iclark(ctl_vector3_t *ab0, GMP_CTL_OUTPUT_TAG ctl_vector3_t *abc)
+void ctl_ct_iclark(
+    // alpha beta 0
+    ctl_vector3_t *ab0,
+    // ABC
+    GMP_CTL_OUTPUT_TAG ctl_vector3_t *abc)
 {
     // Pre-calculate intermediate terms for efficiency and clarity.
     ctrl_gt neg_half_alpha = -ctl_div2(ab0->dat[phase_alpha]);
@@ -198,8 +260,13 @@ void ctl_ct_iclark(ctl_vector3_t *ab0, GMP_CTL_OUTPUT_TAG ctl_vector3_t *abc)
 }
 
 // iClarke coordinate axes transform
+// sine modulation
 GMP_STATIC_INLINE
-void ctl_ct_iclark2(ctl_vector2_t *ab, GMP_CTL_OUTPUT_TAG ctl_vector3_t *abc)
+void ctl_ct_iclark2(
+    // alpha beta
+    ctl_vector2_t *ab,
+    // ABC
+    GMP_CTL_OUTPUT_TAG ctl_vector3_t *abc)
 {
     // Pre-calculate intermediate terms for efficiency and clarity.
     ctrl_gt neg_half_alpha = -ctl_div2(ab->dat[phase_alpha]);
@@ -218,9 +285,35 @@ void ctl_ct_iclark2(ctl_vector2_t *ab, GMP_CTL_OUTPUT_TAG ctl_vector3_t *abc)
     abc->dat[phase_C] = neg_half_alpha - beta_term;
 }
 
+GMP_STATIC_INLINE
+void ctl_ct_iclark_to_line(
+    // alpha & beta
+    ctl_vector3_t *ab,
+    // AB & BC line voltage
+    GMP_CTL_OUTPUT_TAG ctl_vector2_t *u_line)
+{
+    // Constants required for the transformation
+    const ctrl_gt k_three_halves = GMP_CONST_3_OVER_2;     // 3/2
+    const ctrl_gt k_sqrt3_div_2 = GMP_CONST_SQRT_3_OVER_2; // sqrt(3)/2
+    const ctrl_gt k_sqrt3 = GMP_CONST_SQRT_3;              // sqrt(3)
+
+    // tex:
+    // $$U_{ab} = \frac{3}{2} U_\alpha - \frac{\sqrt{3}}{2} U_\beta$$
+    u_line->dat[phase_UAB] =
+        ctl_sub(ctl_mul(k_three_halves, ab->dat[phase_alpha]), ctl_mul(k_sqrt3_div_2, ab->dat[phase_beta]));
+
+    // tex:
+    // $$U_{bc} = \sqrt{3} U_\beta$$
+    u_line->dat[phase_UBC] = ctl_mul(k_sqrt3, ab->dat[phase_beta]);
+}
+
 // uab0(alpha-beta-0) to uabc SVPWM modulation result
 GMP_STATIC_INLINE
-void ctl_ct_svpwm_calc(ctl_vector3_t *ab0, GMP_CTL_OUTPUT_TAG ctl_vector3_t *Tabc)
+void ctl_ct_svpwm_calc(
+    // alpha - beta
+    ctl_vector3_t *ab0,
+    // modulation result
+    GMP_CTL_OUTPUT_TAG ctl_vector3_t *Tabc)
 {
     ctrl_gt Ua, Ub, Uc; // Uabc three phase parameters
     ctrl_gt Umax, Umin, Ucom;
@@ -229,7 +322,7 @@ void ctl_ct_svpwm_calc(ctl_vector3_t *ab0, GMP_CTL_OUTPUT_TAG ctl_vector3_t *Tab
     ctrl_gt Ubeta_tmp = ctl_mul(ab0->dat[phase_beta], GMP_CONST_SQRT_3_OVER_2);
 
     // tex: $$
-    //                U_a = U_\alpha, \\
+    //                        U_a = U_\alpha, \\
     //U_b = -U_\alpha /2 + \sqrt{3}/2\cdot U_\beta, \\
     //U_c = -U_\alpha /2 - \sqrt{3}/2\cdot U_\beta,
     //$$
@@ -267,7 +360,11 @@ void ctl_ct_svpwm_calc(ctl_vector3_t *ab0, GMP_CTL_OUTPUT_TAG ctl_vector3_t *Tab
 // uab0(alpha-beta-0) to uabc SVPWM modulation result
 // This an implementation based on theorem.
 GMP_STATIC_INLINE
-void ctl_ct_svpwm_calc_theorem(ctl_vector3_t *ab0, GMP_CTL_OUTPUT_TAG ctl_vector3_t *Tabc)
+void ctl_ct_svpwm_calc_theorem(
+    // alpha beta
+    ctl_vector3_t *ab0,
+    // modulation result
+    GMP_CTL_OUTPUT_TAG ctl_vector3_t *Tabc)
 {
     // u2s: Ualpha ,Ubeta
     ctrl_gt X, Y, Z, T1, T2, Ta, Tb, Tc;
